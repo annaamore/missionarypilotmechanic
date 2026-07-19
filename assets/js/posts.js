812653a673d77f59762d0fa08506b60c25222b19
Anna Johnson <anna@annas-MacBook-Air.local>
2026-07-18 20:34:46 -0600

    Ignore tests/, *.json, playwright.config.js and remove from remote; add blog post navigation

---
+++ assets/js/posts.js
@@ -0,0 +1,8 @@
+const blogPosts = [
+  { url: '/blog/2026/eaa-impact/', title: 'EAA Impact' },
+  { url: '/blog/2026/andrews-university/', title: 'Andrews University' },
+  { url: '/blog/2026/legacy-program/', title: 'Legacy Program' },
+  { url: '/blog/2026/iama-conference/', title: 'IAMA Conference' },
+  { url: '/blog/2026/first-solo/', title: 'First Solo' },
+  { url: '/blog/2025/oshkosh/', title: 'OshKosh' },
+];
--- playwright.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@playwright/test');
-const path = require('path');
-
-module.exports = defineConfig({
-  testDir: 'tests',
-  use: {
-    headless: true,
-  },
-});
